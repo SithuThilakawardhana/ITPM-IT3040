1f9e2cc45126e515806a62f3222010aec7fb7d27
resh0223 <it20646974@my.sliit.lk>
2023-04-27 21:52:14 +0530

    updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,24 @@
 
 import './App.css';
-import Footer from './components/Footer/Footer';
-import Header from './components/Header/Header';
+// import Footer from './components/Footer/Footer';
+// import Header from './components/Header/Header';
 
-// function App() {
-//   return (
-//     <div className="App"> hellow world
-      
-//     </div>
-//   );
-// }
+const App = () => {
+  return (
+    <>
+    <h1>DoNation portal</h1>
+     </>
+  )
+}
 
-const App = () =>(
-  <>
-  <Header/>
-  <main style={{minHeight:"93vh"}}></main>
-  <Footer/>
-  </>
-)
+
+// const App = () =>(
+//   <>
+//   <Header/>
+//   <main style={{minHeight:"93vh"}}></main>
+//   <Footer/>
+//   </>
+// )
+
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import "./bootstrap.min.css";
+// import reportWebVitals from './reportWebVitals';
+// import "./bootstrap.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -13,4 +13,4 @@ root.render(
 );
 
 
-reportWebVitals();
+//reportWebVitals();
